0cab0e3122e986156f47a6025d408f3c42d2de30
D.C. Pagan <dcpagan@posteo.net>
2025-04-07 20:12:53 -0400

    Knapsack

---
+++ knapsack/src/Knapsack.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Knapsack (maximumValue) where
+
+import Control.Lens
+import Data.Array.MArray
+import Data.Array.ST
+import Data.Foldable
+import Data.List
+import qualified Data.Set as S
+
+data Item where
+  Item ::
+    { _weight :: Int
+    , _value :: Int
+    , _serial :: Int
+    } -> Item
+  deriving (Eq, Ord, Show)
+makeLenses ''Item
+
+type Knapsack = S.Set Item
+
+toKnapsack :: [(Int, Int)] -> Knapsack
+toKnapsack =
+  S.fromList . zipWith (\_serial (_weight, _value) -> Item { .. }) (enumFrom 0)
+
+maximumValue :: Int -> [(Int, Int)] -> Int
+maximumValue capacity items = sumOf (ix capacity . folded . value)
+  $ runSTArray (knapsack capacity $ toKnapsack items)
+
+knapsack :: MArray a Knapsack m
+  => Int -> Knapsack -> m (a Int Knapsack)
+knapsack capacity items = do
+  z <- newArray (0, capacity) S.empty
+  foldrM f z $ sort $ S.toList items
+  where
+    f :: MArray a Knapsack m => Item -> a Int Knapsack -> m (a Int Knapsack)
+    f item@Item { .. } a =
+      a <$ (getAssocs a >>= mapM_ (\(i, e) -> g i e >>= writeArray a i))
+      where
+        g cap withoutItem =
+          if cap < _weight
+          then return withoutItem
+          else do
+            withItem <- S.insert item <$> readArray a (cap - _weight)
+            return $
+              if sumOf (folded . value) withItem
+                > sumOf (folded . value) withoutItem
+              then withItem
+              else withoutItem
