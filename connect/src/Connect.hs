b682d5a955bd6a81ea41a216ebe538d981509870
D.C. Pagan <dcpagan@posteo.net>
2025-10-20 08:50:03 -0400

    WIP: Connect - add array

--- connect/src/Connect.hs
+++ connect/src/Connect.hs
@@ -1,17 +1,21 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
-{-# OPTIONS_GHC -Wno-unused-do-bind #-}
 
 module Connect (Mark(..), winner) where
 
 import Control.Applicative (Alternative(..), Applicative(..))
+import Control.Arrow (Arrow(..))
 import Control.Monad
 
-import Data.List (intersperse)
+import Data.Array.IArray
+import Data.Array.MArray
+import Data.Function (on)
+import Data.List (groupBy, intersperse, uncons)
 
 import Text.ParserCombinators.ReadP hiding (many)
 import Text.ParserCombinators.ReadPrec (lift)
@@ -23,7 +27,7 @@ data Mark
   | Empty
   deriving (Eq, Enum, Bounded)
 
-type Hex = [[Mark]]
+type Hex = Array (Int, Int) Mark
 
 toMark :: Char -> Maybe Mark
 toMark = \case
@@ -54,22 +58,32 @@ row i j = do
 newline :: ReadP ()
 newline = void $ char '\n'
 
-parallelogram :: ReadP Hex
-parallelogram = do
+toAssocs :: Integral i => [[a]] -> [((i, i), a)]
+toAssocs =
+  concat . zipWith (fmap . first . (,)) (enumFrom 0) . fmap (zip (enumFrom 0))
+
+toAssocRows :: Ix i => Array (i, i) a -> [[a]]
+toAssocRows = fmap (fmap snd) . groupBy (on (==) (fst . fst)) . assocs
+
+hex :: ReadP Hex
+hex = do
   r <- marks
   newline
-  rs <- parallelogram' (length r) 1 <|> [] <$ eof
-  return $ r : rs
+  rs <- parallelogram (length r) 1 <|> [] <$ eof
+  let rows = r : rs
+      len = length rows
+      wid = maybe 0 (length . fst) $ uncons rows
+  return $ array ((0, 0), (pred len, pred wid)) $ toAssocs rows
   where
-    parallelogram' :: Int -> Int -> ReadP Hex
-    parallelogram' i j = do
+    parallelogram :: Int -> Int -> ReadP [[Mark]]
+    parallelogram i j = do
       r <- row i j
       newline
-      rs <- parallelogram' i (succ j) <|> [] <$ eof
+      rs <- parallelogram i (succ j) <|> [] <$ eof
       return $ r : rs
 
 instance {-# OVERLAPPING #-}Read Hex where
-  readPrec = lift parallelogram
+  readPrec = lift hex
   readListPrec = many readPrec
 
 instance Show Mark where
@@ -77,7 +91,9 @@ instance Show Mark where
   showList = (++) . intersperse ' ' . fmap fromMark
 
 instance {-# OVERLAPPING #-}Show Hex where
-  show = unlines . zipWith ((++) . flip replicate ' ') (enumFrom 0) . fmap show
+  show =
+    unlines . zipWith ((++) . flip replicate ' ') (enumFrom 0) . fmap show
+    . toAssocRows
   showList = (++) . unlines . fmap show
 
 winner :: [String] -> Maybe Mark
